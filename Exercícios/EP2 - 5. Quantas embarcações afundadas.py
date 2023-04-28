a21c17dae0613e5e7978cdf85aded337b336a151
Gustavo Takahashi <gustavoet@al.insper.edu.br>
2023-04-28 10:55:05 -0300

    Ex 5

--- Exercícios/EP2 - 5. Quantas embarcações afundadas.py
+++ Exercícios/EP2 - 5. Quantas embarcações afundadas.py
@@ -1,1 +1,10 @@
-def afundados(frota,tabuleiro):
+def afundados(frota, tabuleiro):
+    num_afundados = 0
+    for embarcacao, posicoes in frota.items():
+        for posicao in posicoes:
+            for linha, coluna in posicao:
+                if tabuleiro[linha][coluna] != 'X':
+                    break
+            else:
+                num_afundados += 1
+    return num_afundados
